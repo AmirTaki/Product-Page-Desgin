418a0ab82a9262c1be0e42ac96eecf75fb5c6943
amir <amirtaki623@gmail.com>
2025-05-22 23:43:28 +0330

    js : anthoer onclick btn

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 let productImg = document.querySelector("#productImg")
 let btn = document.querySelectorAll(".btn")
 
-console.log(btn)
+
 btn[0].onclick = () => {
     productImg.src = "./src/img/image1.png"   
+}
+
+btn[1].onclick = () => {
+    productImg.src = "./src/img/image2.png"   
+}
+
+btn[2].onclick = () => {
+    productImg.src = "./src/img/image3.png"   
 }
