1b1862e519e2c8ac5fa261a71e98ffd8ce3c4cfe
amir <amirtaki623@gmail.com>
2025-05-22 23:48:44 +0330

    js : add btn color for select

--- src/main.js
+++ src/main.js
@@ -4,12 +4,27 @@ let btn = document.querySelectorAll(".btn")
 
 btn[0].onclick = () => {
     productImg.src = "./src/img/image1.png"   
+    for(bt of btn){
+        bt.classList.remove('active')
+    }
+    console.log(btn[0].classList)
+      btn[0].className = "btn active"
 }
+
 
 btn[1].onclick = () => {
     productImg.src = "./src/img/image2.png"   
+        for(bt of btn){
+        bt.classList.remove('active')
+    }
+    btn[1].className = "btn active"
 }
+
 
 btn[2].onclick = () => {
     productImg.src = "./src/img/image3.png"   
+    for(bt of btn){
+        bt.classList.remove('active')
+    }
+     btn[2].className = "btn active"
 }
